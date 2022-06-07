616c9e903a777c255a62542da46ad4a019224dd7
TusharKRN <krntushar1217@gmail.com>
2022-06-08 00:46:37 +0530

    HackerRank Day-1

---
+++ playingwithcharacters.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char ch,s[100],sen[100];
+    scanf("%c",&ch);
+    scanf("%s",s);
+    scanf("\n");
+    scanf("%[^\n]s", sen);
+    printf("%c\n%s\n%s",ch,s,sen);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
